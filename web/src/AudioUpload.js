a395247c8cb06d8795e710546d91a5a41c72bce8
tianjin <toctan@users.noreply.github.com>
2020-03-05 09:38:12 -0500

    Add FoursquareSuggest component

--- web/src/AudioUpload.js
+++ web/src/AudioUpload.js
@@ -1,24 +1,18 @@
 import { Box, Container, Typography } from '@material-ui/core';
 import React from 'react';
 
-class AudioUpload extends React.Component {
+import FoursquareSuggest from './FoursquareSuggest';
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
 
-  render() {
-    return (
-      <Container component="main" maxWidth="xs">
-        <Box mt={8} align="center">
-          <Typography component="h1" variant="h4">
-            Upload
-          </Typography>
-        </Box>
-      </Container>
-    );
-  }
-}
-
-export default AudioUpload;
+export default function AudioUpload() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box mt={8}>
+        <Typography component="h1" variant="h5">
+          Attach to a venue
+        </Typography>
+        <FoursquareSuggest />
+      </Box>
+    </Container>
+  );
+};
---
+++ web/src/FoursquareSuggest.js
@@ -0,0 +1,117 @@
+import { Autocomplete } from '@material-ui/lab';
+import {
+  CircularProgress,
+  Grid,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { throttle } from 'throttle-debounce';
+import React from 'react';
+
+import CategoryIcon from './CategoryIcon';
+import foursquare from './APIClient.js';
+
+
+const getCurrentLocation = (options) => {
+  return new Promise((resolve, reject) => {
+    const success = (pos) => {
+      const { latitude, longitude } = pos.coords;
+      return resolve(`${latitude},${longitude}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(
+      success, reject, {
+        // enableHighAccuracy: true,
+        maximumAge: 600 * 1000,
+      }
+    );
+  });
+};
+
+
+export default function FoursquareSuggest() {
+  const [loading, setLoading] = React.useState(false);
+  const [venues, setVenues] = React.useState([]);
+  const [selected, setSelected] = React.useState([]);
+
+  const fetchData = React.useMemo(
+    () =>
+      throttle(300, (query) => {
+        getCurrentLocation().then(location => {
+          foursquare.get('/venues/suggestcompletion', {
+            params: {
+              ll: location,
+              query: query,
+              limit: 5
+            }
+          })
+            .then(resp => {
+              setVenues(resp.data.response.minivenues);
+            })
+            .catch(console.log)
+            .then(() => {
+              setLoading(false);
+            });
+        });
+      }),
+    [],
+  );
+
+  const handleSuggest = (event) => {
+    const query = event.target.value.trim();
+    setVenues([]);
+    setLoading(Boolean(query));
+    query && fetchData(query);
+  };
+
+  const handleSelected = (_, venue) => {
+    setSelected([venue && venue.id]);
+  };
+
+  return (
+    <Autocomplete
+      options={venues}
+      getOptionLabel={option => option.name}
+      autoComplete
+      includeInputInList
+      disableOpenOnFocus
+      onChange={handleSelected}
+      loading={loading}
+      renderInput={params => (
+        <TextField
+          {...params}
+          margin="normal"
+          label="Search Foursquare"
+          variant="outlined"
+          fullWidth
+          onChange={handleSuggest}
+          onFocus={getCurrentLocation}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+      renderOption={venue => {
+        return (
+          <Grid container alignItems="center" key={venue.id} spacing={2} >
+            <Grid item>
+              <CategoryIcon category={venue.categories[0]} />
+            </Grid>
+            <Grid item xs zeroMinWidth>
+              <Typography noWrap>{venue.name}</Typography>
+              <Typography variant="body2" color="textSecondary">
+                {venue.location.address}
+              </Typography>
+            </Grid>
+          </Grid>
+        );
+      }}
+    />
+  );
+}
