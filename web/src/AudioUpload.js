ec7d5a1b1f66d77402e4bda9347e90de20b29cc9
tianjin <toctan@users.noreply.github.com>
2020-03-05 17:52:03 -0500

    Add material-ui-dropzone to support drag 'n' drop upload

--- web/src/AudioUpload.js
+++ web/src/AudioUpload.js
@@ -1,10 +1,13 @@
-import { Box, Container, Typography } from '@material-ui/core';
+import { Box, Container, Divider, Typography } from '@material-ui/core';
+import { DropzoneArea } from 'material-ui-dropzone';
 import React from 'react';
 
 import FoursquareSuggest from './FoursquareSuggest';
 
 
 export default function AudioUpload() {
+  const [files, setFiles] = React.useState([]);
+
   return (
     <Container component="main" maxWidth="xs">
       <Box mt={8}>
@@ -12,6 +15,19 @@ export default function AudioUpload() {
           Attach to a venue
         </Typography>
         <FoursquareSuggest />
+
+        <Divider variant="middle" />
+        <Typography component="h1" variant="h5" gutterBottom >
+          Upload
+        </Typography>
+
+        <DropzoneArea
+          dropzoneText={"Drag 'n' drop, or click to select"}
+          onChange={setFiles}
+          acceptedFiles={['audio/*']}
+          filesLimit={1}
+        />
+
       </Box>
     </Container>
   );
